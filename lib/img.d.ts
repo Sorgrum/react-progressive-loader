a35ab523be6b17934e8484ae772ff530c0574cc3
Marcelo Gheiler <marcelogheiler@gmail.com>
2019-02-04 00:13:01 -0500

    rebuild

--- lib/img.d.ts
+++ lib/img.d.ts
@@ -3,6 +3,7 @@ export declare function Img(props: HTMLAttributes<any> & {
     src: string;
     placeholderSrc?: string;
     bgColor?: string;
+    aspectRatio: number;
     loadOnScreen?: boolean;
     blurAmount?: number;
 }): JSX.Element;
